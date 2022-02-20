c7485539c5d77b9767fb3a5cd11a49a71bf2725c
Kgaogel09 <kgaogelo@swipeix.com>
2022-02-20 19:04:47 +0200

    feat(component):first styled component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+
+const BasicTitle = styled.h2`
+  text-align: center;
+  text-transform: capitalize;
+  color: green;
+`;
+
 function App() {
-  return <h2>Styled Components</h2>;
+  return (
+    <div style={{ padding: "2rem" }}>
+      <BasicTitle>Styled Components</BasicTitle>
+      <button className="btn">Click Me</button>
+    </div>
+  );
 }
 
 export default App;
